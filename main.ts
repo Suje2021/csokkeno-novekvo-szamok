e655f1076c4b2352668923e4ef660da16f8c75db
Suje2021 <80900194+Suje2021@users.noreply.github.com>
2021-04-22 17:01:36 +0200

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    x += 1
+    basic.showNumber(x)
+})
+input.onButtonPressed(Button.B, function () {
+    x += -1
+    basic.showNumber(x)
+})
+let x = 0
+x = 0
